e064cf428fc318245052a7a435d519438cd2ad11
Ashimoro <fl34un@gmail.com>
2025-03-20 22:13:11 -0400

    Condition checker and list of "trash" object is created

--- BTExperiments/Assets/Scripts/C_TrashListChecker.cs
+++ BTExperiments/Assets/Scripts/C_TrashListChecker.cs
@@ -1,13 +1,17 @@
+using System.Collections.Generic;
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using Unity.VisualScripting;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Conditions {
 
 	public class C_TrashListChecker : ConditionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
+		public BBParameter<List<GameObject>> trash;
+
+
 		protected override string OnInit(){
 			return null;
 		}
@@ -25,7 +29,16 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+			GameObject[] trashSearcher = GameObject.FindGameObjectsWithTag("Trash");
+			trash.value.Clear();
+
+			foreach (GameObject obj in trashSearcher) {
+				trash.value.Add(obj);
+			}
+
+			return trash.value.Count > 0;
+
+
 		}
 	}
 }
