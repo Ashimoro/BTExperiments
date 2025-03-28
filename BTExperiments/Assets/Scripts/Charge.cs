f9bbfaf77e6f0822504af8495b24f63f71ec1bf8
Ashimoro <fl34un@gmail.com>
2025-03-27 21:48:15 -0400

    Everything is done

--- BTExperiments/Assets/Scripts/Charge.cs
+++ BTExperiments/Assets/Scripts/Charge.cs
@@ -9,42 +9,43 @@ namespace NodeCanvas.Tasks.Actions {
 
 	public class Charge : ActionTask {
 
-		
+			// BBParameters:
 		public BBParameter<GameObject> playerLocation;
 		public BBParameter<GameObject> fireEyes;
 		public BBParameter<float> movementSpeed;
 
+			// Everything related to back steps before charge:
 		private float _backDistance = 2f;
 		private Vector3 _backPosition;
 		private bool _backComplete;
-		private float _distanceThreshold = 0.5f;
-
+		
+			// Everything related to player:
 		private Vector3 _dashTarget;
 
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
+			// Misc:
+		private float _distanceThreshold = 0.5f;
+		
 		protected override void OnExecute() {
 			
-			fireEyes.value.SetActive(true);
+			fireEyes.value.SetActive(true); // Turn on the fiery eyes
 
-			Vector3 rotationDirection = playerLocation.value.transform.position - agent.transform.position;
-			rotationDirection.y = 0;
-			agent.transform.rotation = Quaternion.LookRotation(rotationDirection); //Поворачиваем рыцаря по направлению к игроку. Это находится тут а не в 
+			Vector3 rotationDirection = playerLocation.value.transform.position - agent.transform.position; // Set the position where to turn
+			rotationDirection.y = 0; // Make the rotation ignore the y coordinate. This is just in case
+			agent.transform.rotation = Quaternion.LookRotation(rotationDirection); // Turn the knight towards the player.
 
-			_backComplete = false;
-			_backPosition = agent.transform.position - agent.transform.forward * _backDistance;
+			_backComplete = false; // This must be reset for the charge attack to work properly
+			_backPosition = agent.transform.position - agent.transform.forward * _backDistance; // The point to which the knight will retreat before the charge attack.
 
-			_dashTarget = playerLocation.value.transform.position;
+			_dashTarget = playerLocation.value.transform.position; // The last position of the player, which was when the script was activated
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
 			
 			if (!_backComplete) {
-				agent.transform.position = Vector3.MoveTowards(agent.transform.position, _backPosition, movementSpeed.value / 5 * Time.deltaTime);
+				agent.transform.position = Vector3.MoveTowards(agent.transform.position, _backPosition, movementSpeed.value / 5 * Time.deltaTime); // Slowly pulling back
 
-					if(Vector3.Distance(agent.transform.position, _backPosition) < _distanceThreshold){
+					if(Vector3.Distance(agent.transform.position, _backPosition) < _distanceThreshold){ // Check the distance to the charge point
 						_backComplete = true;
 					} 
 
@@ -52,9 +53,10 @@ namespace NodeCanvas.Tasks.Actions {
 
 			
 			if (playerLocation.value != null && _backComplete == true){
-				agent.transform.position = Vector3.MoveTowards(agent.transform.position, _dashTarget, movementSpeed.value * Time.deltaTime);
+				agent.transform.position = Vector3.MoveTowards(agent.transform.position, _dashTarget, movementSpeed.value * Time.deltaTime); 
+				// ^^ Move towards the player's last position ^^
 
-					if(Vector3.Distance(agent.transform.position, _dashTarget) < _distanceThreshold + 0.6){
+					if(Vector3.Distance(agent.transform.position, _dashTarget) < _distanceThreshold + 0.6){ // Distance check for stopping + 0.6, due to the size of the player model
 						EndAction(true);
 					}
 			}
