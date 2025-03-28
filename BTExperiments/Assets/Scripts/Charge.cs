eb2d722dd32c968250e88026e4907920f51facd8
Ashimoro <fl34un@gmail.com>
2025-03-27 21:07:37 -0400

    Backing working, Dashing not working

--- BTExperiments/Assets/Scripts/Charge.cs
+++ BTExperiments/Assets/Scripts/Charge.cs
@@ -15,10 +15,11 @@ namespace NodeCanvas.Tasks.Actions {
 		public BBParameter<float> movementSpeed;
 
 		private float _backDistance = 2f;
+		private Vector3 _backPosition;
+		private bool _backComplete;
 
-		protected override string OnInit() {
-			return null;
-		}
+
+		private float _distanceThreshold = 0.1f;
 
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
@@ -31,13 +32,32 @@ namespace NodeCanvas.Tasks.Actions {
 			rotationDirection.y = 0;
 			agent.transform.rotation = Quaternion.LookRotation(rotationDirection);
 
-			EndAction(true);
+			_backComplete = false;
+			_backPosition = agent.transform.position - agent.transform.forward * _backDistance;
 
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
 			
+			if (!_backComplete) {
+				agent.transform.position = Vector3.MoveTowards(agent.transform.position, _backPosition, movementSpeed.value / 2 * Time.deltaTime);
+
+					if(Vector3.Distance(agent.transform.position, _backPosition) < _distanceThreshold){
+						_backComplete = true;
+					}
+
+			if (playerLocation.value != null && _backComplete == true){
+				Vector3 _dashTarget = playerLocation.value.transform.position;
+
+				agent.transform.position = Vector3.MoveTowards(agent.transform.position, _dashTarget, movementSpeed.value * Time.deltaTime);
+
+					if(Vector3.Distance(agent.transform.position, _dashTarget) < _distanceThreshold){
+						EndAction(true);
+					}
+			}
+
+			} 
 			
 
 
