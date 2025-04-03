a373962725a22c1e50390d0e08dad864a2c722ed
Ashimoro <fl34un@gmail.com>
2025-04-02 21:33:28 -0400

    Player movement updated. Code now has comments

--- BTExperiments/Assets/Scripts/Player Movement.cs
+++ BTExperiments/Assets/Scripts/Player Movement.cs
@@ -6,27 +6,68 @@ public class PlayerMovement : MonoBehaviour
 {
     
     public float speed = 5f;
+    public float jumpForce = 5f;
+    
     private float _rotationSpeed = 720f;
+    private Rigidbody _rb;
+    private bool _isGrounded;
+    private Vector3 _movementDirection;
+    
+        void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
-    // Update is called once per frame
+
     void Update()
     {
         
         float horizontaInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 _direction = new Vector3(horizontaInput, 0f, verticalInput);
+        _movementDirection = new Vector3(horizontaInput,0f,verticalInput); // Getting palyer inputs into a single vector3 variable
 
-        if (_direction.magnitude > 0.1f) {
 
-            _direction.Normalize();
 
-            transform.Translate(_direction * speed * Time.deltaTime, Space.World);
-            Quaternion _rotation = Quaternion.LookRotation(_direction,Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, _rotation, _rotationSpeed * Time.deltaTime);
+        if (Input.GetKeyDown("space") && _isGrounded) {
+
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Player jumping
 
         }
 
+    }
 
+
+    void FixedUpdate()
+    {
+        if (_movementDirection.magnitude > 0.1f) {
+            Vector3 move = _movementDirection * speed * Time.fixedDeltaTime; // Adding to the inputs player speed and using time.fixedDeltaTime, to make everything smoother;
+            _rb.MovePosition(_rb.position + move); // Player movement
+
+            Quaternion targetPosition = Quaternion.LookRotation(_movementDirection, Vector3.up); //Finding position where player should be rotated
+            Quaternion rotation = Quaternion.RotateTowards(_rb.rotation, targetPosition, _rotationSpeed * Time.fixedDeltaTime); //Making rotation smoother and not instant
+            _rb.MoveRotation(rotation); //Rotating player to the direction of movement
+
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+
+            _isGrounded = true;
+
+        }
+    }
+
+
+    void OnCollisionExit(Collision collision)
+    {
+         if (collision.gameObject.CompareTag("Ground")){
+
+            _isGrounded = false;
+
+        }
     }
 }
